c3b21858717769256b15fa06d5e2f84dab882717
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-08 01:43:42 +0530

    UC1

---
+++ UC1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmpWage
+{
+    public class UC1
+    {
+
+
+        public static void Attendance()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
