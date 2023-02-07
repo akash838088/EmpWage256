0e60eb6470a59bf33e465dddd3c91c989039881d
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-08 01:47:01 +0530

    uc2

---
+++ UC2.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace EmpWage
+{
+    public class UC2
+    {
+
+        public static void EmpWage()
+        {
+            int Is_Full_Time = 1;
+            int Emp_Rate_Per_Hour = 20;
+
+            int Empwage;
+            int Emphours;
+
+            Random random = new Random();
+            int empcheck = random.Next(2);
+
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Full Time");
+                Emphours = 0;
+            }
+            else
+
+                Emphours = 8;
+
+            Empwage = Emphours * Emp_Rate_Per_Hour;
+
+            Console.WriteLine("Daily employee wage is : " + Empwage);
+
+
+        }
+    }
+}
