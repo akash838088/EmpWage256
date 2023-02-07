f0faa2952febfe20a6439bfb35cb9b421e1f489b
kinnarypatel93 <69387654+kinnarypatel93@users.noreply.github.com>
2023-02-08 01:41:36 +0530

    [Akash] Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+using System;
+
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpCheck empcheck = new EmpCheck();
+            EmpCheck.Attendance();
+
+            DailyWage empcheckk = new DailyWage();
+            DailyWage.EmpWage();
+
+            PartTime empcheckkk = new PartTime();
+            PartTime.EmpWagePart();
+            Console.ReadLine();
+
+            Switchcase emppcheck = new Switchcase();
+            Switchcase.SwitchCase();
+        }
+    }
+}
